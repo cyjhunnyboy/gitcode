c7eb11621ffed95c709c13eeb2e3dbe4b1adf782
cyjhunnyboy <cyjhunnyboy@sina.com>
2019-10-02 00:18:21 -0400

    为HelloWorld.java、SayHello.java添加JavaDoc注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 package per.cyj.demo;
 
+/**
+ * @author chenyongjun
+ * @apiNote Hello World Demo
+ * @since 2019-10-02
+ */
 public class HelloWorld {
 
     public static void main(String[] args) {
