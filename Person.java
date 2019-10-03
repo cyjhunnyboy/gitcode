35e21605bda4ce45176bf49329d4e270e99d7569
cyjhunnyboy <cyjhunnyboy@sina.com>
2019-10-03 06:32:45 -0400

    Person类添加Java Doc注释

--- Person.java
+++ Person.java
@@ -8,14 +8,33 @@ package per.cyj.demo;
  */
 public class Person {
 
+    /**
+     * 姓名
+     */
     private String name;
+
+    /**
+     * 年龄
+     */
     private Integer age;
+
+    /**
+     * 性别
+     */
     private String sex;
 
+    /**
+     * 无参构造函数
+     */
     public Person(){
     	super();
     }
 
+    /**
+     * @param name 姓名
+     * @param age 年龄
+     * @param sex 性别
+     */
     public Person(String name, Integer age, String sex) {
     
         this.name = name;
